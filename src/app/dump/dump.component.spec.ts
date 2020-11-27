5527184810e80880584890dd643bcdc987958ea1
Alex <alexfomin2402@gmail.com>
2020-11-27 21:18:32 +0300

    test telegram login widget

---
+++ src/app/dump/dump.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { DumpComponent } from './dump.component';
+
+describe('DumpComponent', () => {
+  let component: DumpComponent;
+  let fixture: ComponentFixture<DumpComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ DumpComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(DumpComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
