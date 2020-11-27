da76fbc79bb987d2a0c082ea16f1398cb432185b
Alex <alexfomin2402@gmail.com>
2020-11-27 21:18:32 +0300

    test telegram login widget

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { DumpComponent } from './dump/dump.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    component: DumpComponent,
+    path: '**',
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
