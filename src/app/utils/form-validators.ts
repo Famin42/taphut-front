8d1b95d87b2fbc767deef6ef3df736b3c1b21f05
Alex <alexfomin2402@gmail.com>
2021-04-14 19:37:41 +0300

    feat: add custom validation

--- src/app/utils/form-validators.ts
+++ src/app/utils/form-validators.ts
@@ -1,4 +1,4 @@
-import { Validators } from '@angular/forms';
+import { AbstractControl, ValidatorFn, Validators } from '@angular/forms';
 
 export const PASSWORD_VALIDATORS = [
   Validators.required,
@@ -6,3 +6,17 @@ export const PASSWORD_VALIDATORS = [
   // Validators.pattern("[\\w\\[\\]`!@#$%\\^&*()={}:;<>+'-]*"),
 ];
 export const EMAIL_VALIDATORS = [Validators.required, Validators.email];
+
+export function greaterThan(main: string, fieldToCompare: string): ValidatorFn {
+  return (control: AbstractControl): { [key: string]: any } => {
+    console.log(control);
+    const mainValue = control?.get(main)?.value;
+    const valueToCompare = control?.get(fieldToCompare)?.value;
+    const isLessThen =
+      typeof valueToCompare === 'number' &&
+      typeof mainValue === 'number' &&
+      mainValue < valueToCompare;
+
+    return isLessThen ? { lessThan: { actual: mainValue, compare: valueToCompare } } : {};
+  };
+}
