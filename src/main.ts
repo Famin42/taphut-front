4fde34fe1f0ca96c103a7161e94b88b62a809fcc
Alex <alexfomin2402@gmail.com>
2021-04-16 01:13:48 +0300

    feat: add amplify auth configuring

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,11 @@
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { enableProdMode } from '@angular/core';
-import Amplify from 'aws-amplify';
+import { Auth } from 'aws-amplify';
 
 import { environment } from './environments/environment';
 import { AppModule } from './app/app.module';
 
-Amplify.configure(environment.AmplifyConfig);
+Auth.configure(environment.AmplifyConfig);
 
 if (environment.production) {
   enableProdMode();
@@ -14,11 +14,3 @@ if (environment.production) {
 platformBrowserDynamic()
   .bootstrapModule(AppModule)
   .catch((err) => console.error(err));
-
-//
-// If you uncomment this it will fail:
-// Amplify.configure(AmplifyConfig);
-// Auth.configure(AmplifyConfig);
-
-// import awsconfig from './aws-exports';
-// Amplify.configure(awsconfig);
